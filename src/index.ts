7b15a47679869b1a67bed6953672a3ec19d1c7dd
LeomharT <leo.liao.lzy@gmail.com>
2025-09-15 09:58:15 +0800

    feat: rotation with mouse perfect

--- src/index.ts
+++ src/index.ts
@@ -1,244 +1,41 @@
-import {
-	BackSide,
-	Clock,
-	Color,
-	IcosahedronGeometry,
-	Mesh,
-	MeshBasicMaterial,
-	PCFSoftShadowMap,
-	PerspectiveCamera,
-	Scene,
-	ShaderChunk,
-	ShaderMaterial,
-	SphereGeometry,
-	Spherical,
-	SRGBColorSpace,
-	TextureLoader,
-	Uniform,
-	Vector2,
-	Vector3,
-	WebGLRenderer,
-} from 'three';
-import {
-	GLTFLoader,
-	OrbitControls,
-	RGBELoader,
-	TrackballControls,
-} from 'three/examples/jsm/Addons.js';
-import Stats from 'three/examples/jsm/libs/stats.module.js';
-import { Pane } from 'tweakpane';
-import atmosphereFragmentShader from './shader/atmosphere/fragment.glsl?raw';
-import atmosphereVertexShader from './shader/atmosphere/vertex.glsl?raw';
-import earthFragmentShader from './shader/earth/fragment.glsl?raw';
-import earthVertexShader from './shader/earth/vertex.glsl?raw';
-import random2D from './shader/include/random2D.glsl?raw';
-import simplex3DNoise from './shader/include/simplex3DNoise.glsl?raw';
 import './style.css';
 
-type ShaderLab = typeof ShaderChunk & {
-	random2D: string;
-	simplex3DNoise: string;
-};
+// Root el
+const root = document.querySelector('#root') as HTMLDivElement;
+// Box el
+const box = document.querySelector('.box') as HTMLDivElement;
 
-(ShaderChunk as ShaderLab)['random2D'] = random2D;
-(ShaderChunk as ShaderLab)['simplex3DNoise'] = simplex3DNoise;
-
-const el = document.querySelector('#root') as HTMLDivElement;
-
-const sizes = {
-	width: window.innerWidth,
-	height: window.innerHeight,
-	pixelratio: Math.min(2, window.devicePixelRatio),
-	resolution: new Vector2(window.innerWidth, window.innerHeight),
-};
-
-/**
- * Loader
- */
-
-const textureLoader = new TextureLoader();
-textureLoader.setPath('/src/assets/textures/');
-
-const gltfLoader = new GLTFLoader();
-gltfLoader.setPath('/src/assets/models/');
-
-const rgbeLoader = new RGBELoader();
-rgbeLoader.setPath('/src/assets/hdr/');
-
-/**
- * Textures
- */
-
-const earthDayMapTexture = textureLoader.load('2k_earth_daymap.jpg');
-earthDayMapTexture.colorSpace = SRGBColorSpace;
-earthDayMapTexture.anisotropy = 8;
-
-const earthNightMapTexture = textureLoader.load('2k_earth_nightmap.jpg');
-earthNightMapTexture.colorSpace = SRGBColorSpace;
-earthNightMapTexture.anisotropy = 8;
-
-const specularCloudTexutre = textureLoader.load('specularClouds.jpg');
-
-/**
- * Basic
- */
-
-const renderer = new WebGLRenderer({
-	alpha: true,
-	antialias: true,
-});
-renderer.setSize(sizes.width, sizes.height);
-renderer.setPixelRatio(1.0);
-// renderer.toneMapping = ACESFilmicToneMapping;
-renderer.toneMappingExposure = 1.0;
-renderer.shadowMap.enabled = true;
-renderer.shadowMap.type = PCFSoftShadowMap;
-el.append(renderer.domElement);
-
-const scene = new Scene();
-scene.background = new Color('#1e1e1e');
-
-const camera = new PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 1000);
-camera.position.set(3, 3, 3);
-camera.lookAt(scene.position);
-
-const clock = new Clock();
-
-const controls = new OrbitControls(camera, renderer.domElement);
-controls.enableDamping = true;
-controls.enableRotate = true;
-controls.enablePan = false;
-controls.enableZoom = false;
-// controls.maxPolarAngle = Math.PI / 2.25;
-// controls.minPolarAngle = 0;
-
-const controls2 = new TrackballControls(camera, renderer.domElement);
-controls2.noRotate = true;
-controls2.noPan = true;
-controls2.noZoom = false;
-controls2.dynamicDampingFactor = 0.2;
-
-const stats = new Stats();
-el.append(stats.dom);
-
-/**
- * Uniforms
- */
-
-const uniforms = {
-	uSunDirection: new Uniform(new Vector3()),
-
-	uEarthDayMapTexture: new Uniform(earthDayMapTexture),
-	uEarthNightMapTexture: new Uniform(earthNightMapTexture),
-	uSpecularCloudTexture: new Uniform(specularCloudTexutre),
-
-	uAtmosphereDayColor: new Uniform(new Color('#00aaff')),
-	uAtmosphereTwilightColor: new Uniform(new Color('#ff6600')),
-};
-
-/**
- * World
- */
-
-const sunSpherical = new Spherical(1.0, Math.PI / 2, 0.5);
-const sunDirection = new Vector3();
-
-function updateSun() {
-	// Sun direction
-	sunDirection.setFromSpherical(sunSpherical);
-
-	// Uniform
-	uniforms.uSunDirection.value.copy(sunDirection);
-
-	// Sun
-	sun.position.copy(sunDirection.clone().multiplyScalar(5.0));
+function onPointerDown() {
+	root.addEventListener('pointermove', onPointerMove);
 }
 
-const sunGeometry = new IcosahedronGeometry(0.1, 3);
-const sunMateiral = new MeshBasicMaterial({ color: 'yellow' });
+function onPointerMove(e: PointerEvent) {
+	root.setPointerCapture(e.pointerId);
 
-const sun = new Mesh(sunGeometry, sunMateiral);
-scene.add(sun);
-updateSun();
+	const boxRect = box.getBoundingClientRect();
 
-const earthGeometry = new SphereGeometry(2, 128, 128);
-const earthMaterial = new ShaderMaterial({
-	vertexShader: earthVertexShader,
-	fragmentShader: earthFragmentShader,
-	uniforms,
-});
+	const center = {
+		x: boxRect.x + boxRect.width / 2,
+		y: boxRect.y + boxRect.height / 2,
+	};
 
-const earth = new Mesh(earthGeometry, earthMaterial);
-scene.add(earth);
+	const coord = {
+		x: e.clientX,
+		y: e.clientY,
+	};
 
-const atmosphereGeometry = earthGeometry.clone();
-const atmosphereMaterial = new ShaderMaterial({
-	vertexShader: atmosphereVertexShader,
-	fragmentShader: atmosphereFragmentShader,
-	uniforms,
-	transparent: true,
-	side: BackSide,
-});
+	const x = coord.x - center.x;
+	const y = coord.y - center.y;
 
-const atmosphere = new Mesh(atmosphereGeometry, atmosphereMaterial);
-atmosphere.scale.setScalar(1.04);
-scene.add(atmosphere);
+	let angle = Math.atan2(y, x);
+	angle *= 180 / Math.PI;
 
-/**
- * Pane
- */
-
-const pane = new Pane({ title: '🚧🚧🚧 Debug Params 🚧🚧🚧' });
-pane.element.parentElement!.style.width = '380px';
-
-pane
-	.addBinding(sunSpherical, 'phi', {
-		min: 0,
-		max: Math.PI,
-		step: 0.001,
-	})
-	.on('change', updateSun);
-
-pane
-	.addBinding(sunSpherical, 'theta', {
-		min: -Math.PI,
-		max: Math.PI,
-		step: 0.001,
-	})
-	.on('change', updateSun);
-
-/**
- * Event
- */
-
-function render() {
-	// Render
-	renderer.render(scene, camera);
-
-	// Time
-	const delta = clock.getDelta();
-
-	// Update
-	controls.update(delta);
-	controls2.update();
-	stats.update();
-
-	earth.rotation.y += delta * 0.1;
-
-	// Animation
-	requestAnimationFrame(render);
-}
-render();
-
-function resize() {
-	sizes.width = window.innerWidth;
-	sizes.height = window.innerHeight;
-	sizes.pixelratio = Math.min(2, window.devicePixelRatio);
-
-	renderer.setSize(sizes.width, sizes.height);
-
-	camera.aspect = sizes.width / sizes.height;
-	camera.updateProjectionMatrix();
+	box.style.transform = `rotate(${angle}deg)`;
 }
 
-window.addEventListener('resize', resize);
+function onPointerUp() {
+	root.removeEventListener('pointermove', onPointerMove);
+}
+
+root.addEventListener('pointerdown', onPointerDown);
+root.addEventListener('pointerup', onPointerUp);
