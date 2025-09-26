527cdc4e8a3af3178619ff622498ba4b8ddcf108
LeomharT <leo.liao.lzy@gmail.com>
2025-09-26 11:38:00 +0800

    faet: bug values need to remember

--- src/index.ts
+++ src/index.ts
@@ -1,47 +1,33 @@
 import {
-	AxesHelper,
+	ACESFilmicToneMapping,
+	BackSide,
 	Clock,
 	Color,
-	DirectionalLight,
-	DoubleSide,
-	EquirectangularReflectionMapping,
-	Layers,
-	Material,
-	MathUtils,
+	IcosahedronGeometry,
 	Mesh,
 	MeshBasicMaterial,
-	MeshDepthMaterial,
-	MeshStandardMaterial,
-	Object3D,
 	PCFSoftShadowMap,
 	PerspectiveCamera,
-	PlaneGeometry,
-	RGBADepthPacking,
 	Scene,
 	ShaderChunk,
 	ShaderMaterial,
 	SphereGeometry,
+	Spherical,
+	SRGBColorSpace,
+	TextureLoader,
 	Uniform,
-	Vector2,
+	Vector3,
 	WebGLRenderer,
 } from 'three';
-import CustomShaderMaterial from 'three-custom-shader-material/vanilla';
-import {
-	EffectComposer,
-	OrbitControls,
-	OutputPass,
-	RenderPass,
-	RGBELoader,
-	ShaderPass,
-	UnrealBloomPass,
-} from 'three/examples/jsm/Addons.js';
+import { OrbitControls, TrackballControls } from 'three/examples/jsm/Addons.js';
 import { Pane } from 'tweakpane';
-import bloomFragmentShader from './shader/bloom/fragment.glsl?raw';
-import bloomVertexShader from './shader/bloom/vertex.glsl?raw';
-import disslutionFragmentShader from './shader/disslution/fragment.glsl?raw';
-import disslutionVertexShader from './shader/disslution/vertex.glsl?raw';
+import atmosphereFragmentShader from './shader/atmosphere/fragment.glsl?raw';
+import atmosphereVertexShader from './shader/atmosphere/vertex.glsl?raw';
+import earthFragmentShader from './shader/earth/fragment.glsl?raw';
+import earthVertexShader from './shader/earth/vertex.glsl?raw';
 import simplex3DNoise from './shader/include/simplex3DNoise.glsl?raw';
 import './style.css';
+
 (ShaderChunk as any)['simplex3DNoise'] = simplex3DNoise;
 
 /**
@@ -54,23 +40,26 @@ const sizes = {
 	pixelratio: Math.min(2.0, window.devicePixelRatio),
 };
 
-const BLOOM_LAYER = 1;
-const layer = new Layers();
-layer.set(BLOOM_LAYER);
-
 /**
  * Loaders
  */
 
-const rgbeLoader = new RGBELoader();
-rgbeLoader.setPath('/src/assets/hdr/');
+const textureLoader = new TextureLoader();
+textureLoader.setPath('/src/assets/textures/');
 
 /**
  * Textures
  */
 
-const environment = await rgbeLoader.loadAsync('rural_evening_road_1k.hdr');
-environment.mapping = EquirectangularReflectionMapping;
+const earthDayMapTexture = textureLoader.load('2k_earth_daymap.jpg');
+earthDayMapTexture.colorSpace = SRGBColorSpace;
+earthDayMapTexture.anisotropy = 8;
+
+const earthNightMapTexture = textureLoader.load('2k_earth_nightmap.jpg');
+earthNightMapTexture.colorSpace = SRGBColorSpace;
+earthNightMapTexture.anisotropy = 8;
+
+const specularCloudTexture = textureLoader.load('specularClouds.jpg');
 
 /**
  * Basic
@@ -82,13 +71,13 @@ const renderer = new WebGLRenderer({
 });
 renderer.setSize(sizes.width, sizes.height);
 renderer.setPixelRatio(sizes.pixelratio);
+renderer.toneMapping = ACESFilmicToneMapping;
 renderer.shadowMap.enabled = true;
 renderer.shadowMap.type = PCFSoftShadowMap;
 el.append(renderer.domElement);
 
 const scene = new Scene();
-scene.background = environment;
-scene.backgroundIntensity = 0.125;
+scene.background = new Color('#1e1e1e');
 
 const camera = new PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 1000);
 camera.position.set(3, 3, 3);
@@ -96,120 +85,76 @@ camera.lookAt(scene.position);
 
 const controls = new OrbitControls(camera, renderer.domElement);
 controls.enableDamping = true;
+controls.enableRotate = true;
+controls.enablePan = false;
+controls.enableZoom = false;
+
+const controls2 = new TrackballControls(camera, renderer.domElement);
+controls2.noPan = true;
+controls2.noRotate = true;
+controls2.noZoom = false;
 
 const clock = new Clock();
-
-/**
- * Post processing
- */
-
-const params = {
-	strength: 0.5,
-	radius: 0.5,
-	threshold: 1.0,
-};
-
-const darkMaterial = new MeshBasicMaterial({ color: 0x000000 });
-const materials: Record<string, Material> = {};
-
-const renderScene = new RenderPass(scene, camera);
-
-const bloomPass = new UnrealBloomPass(
-	new Vector2(sizes.width / 2, sizes.height / 2),
-	params.strength,
-	params.radius,
-	params.threshold
-);
-function updateBloom() {
-	bloomPass.strength = params.strength;
-	bloomPass.radius = params.radius;
-	bloomPass.threshold = params.threshold;
-}
-
-const bloomComposer = new EffectComposer(renderer);
-bloomComposer.renderToScreen = false;
-bloomComposer.addPass(renderScene);
-bloomComposer.addPass(bloomPass);
-
-const mixPass = new ShaderPass(
-	new ShaderMaterial({
-		vertexShader: bloomVertexShader,
-		fragmentShader: bloomFragmentShader,
-		uniforms: {
-			uBaseTexture: new Uniform(null),
-			uBloomTexture: new Uniform(bloomComposer.renderTarget2.texture),
-		},
-	}),
-	'uBaseTexture'
-);
-
-const outputPass = new OutputPass();
-
-const composer = new EffectComposer(renderer);
-composer.addPass(renderScene);
-composer.addPass(mixPass);
-composer.addPass(outputPass);
 
 /**
  * World
  */
 
-const lightColor = new Color(
-	MathUtils.randFloat(0.0, 1.0),
-	MathUtils.randFloat(0.0, 1.0),
-	MathUtils.randFloat(0.0, 1.0)
-);
-
 const uniforms = {
-	uColor: new Uniform(lightColor),
-	uProgress: new Uniform(-1.08),
-	uFrequency: new Uniform(0.85),
+	uSunDirection: new Uniform(new Vector3()),
+	//
+	uEarthDayMapTexture: new Uniform(earthDayMapTexture),
+	uEarthNightMapTexture: new Uniform(earthNightMapTexture),
+	uSpecularCloudTexture: new Uniform(specularCloudTexture),
+	//
+	uAtmosphereDayColor: new Uniform(new Color('#00aaff')),
+	uAtmosphereTwilightColor: new Uniform(new Color('#ff6600')),
 };
+const sunSpherical = new Spherical(1, Math.PI / 2, 0.5);
+const sunDirection = new Vector3();
 
-const sphereGometry = new SphereGeometry(2, 36, 36);
-const sphereMaterial = new ShaderMaterial({
-	vertexShader: disslutionVertexShader,
-	fragmentShader: disslutionFragmentShader,
-	uniforms,
-	side: DoubleSide,
+const sunGeometry = new IcosahedronGeometry(0.1, 3);
+const sunMaterial = new MeshBasicMaterial({
+	color: 'yellow',
 });
 
-const glowSphere = new Mesh(sphereGometry, sphereMaterial);
-glowSphere.castShadow = true;
-glowSphere.layers.enable(BLOOM_LAYER);
-scene.add(glowSphere);
+const sun = new Mesh(sunGeometry, sunMaterial);
+scene.add(sun);
 
-const depthMaterial = new CustomShaderMaterial({
-	baseMaterial: MeshDepthMaterial,
+function updateSun() {
+	// Direction
+	sunDirection.setFromSpherical(sunSpherical);
+
+	// Uniform
+	uniforms.uSunDirection.value.copy(sunDirection);
+
+	// Sun position
+	sun.position.copy(sunDirection.clone().multiplyScalar(3.0));
+}
+updateSun();
+
+const earthGeometry = new SphereGeometry(2, 64, 64);
+const earthMaterial = new ShaderMaterial({
 	uniforms,
-	vertexShader: disslutionVertexShader,
-	fragmentShader: disslutionFragmentShader,
-	depthPacking: RGBADepthPacking,
+	vertexShader: earthVertexShader,
+	fragmentShader: earthFragmentShader,
 });
 
-glowSphere.customDepthMaterial = depthMaterial;
+const earth = new Mesh(earthGeometry, earthMaterial);
+scene.add(earth);
 
-const plane = new Mesh(new PlaneGeometry(15, 15), new MeshStandardMaterial());
-plane.castShadow = true;
-plane.receiveShadow = true;
-plane.rotation.y = Math.PI / 2;
-plane.position.x = -3.0;
-scene.add(plane);
+const atmosphereGeometry = earthGeometry.clone();
+const atmosphereMaterial = new ShaderMaterial({
+	uniforms,
+	vertexShader: atmosphereVertexShader,
+	fragmentShader: atmosphereFragmentShader,
+	transparent: true,
+	side: BackSide,
+});
 
-const axesHelper = new AxesHelper(5);
-scene.add(axesHelper);
-
-/**
- * Light
- */
-
-const directionalLight = new DirectionalLight(0xffffff, 3.0);
-directionalLight.position.x = 5.0;
-directionalLight.position.y = 1.5;
-directionalLight.castShadow = true;
-directionalLight.shadow.mapSize.set(sizes.width, sizes.height);
-
-scene.add(directionalLight);
+const atmosphere = new Mesh(atmosphereGeometry, atmosphereMaterial);
+atmosphere.scale.setScalar(1.04);
+scene.add(atmosphere);
 
 /**
  * Pane
@@ -218,61 +163,37 @@ scene.add(directionalLight);
 const pane = new Pane({ title: 'Debug Params' });
 pane.element.parentElement!.style.width = '380px';
 
-const bP = pane.addFolder({ title: 'Bloom' });
-bP.addBinding(params, 'strength', {
-	min: 0,
-	max: 2,
-	step: 0.001,
-}).on('change', updateBloom);
-bP.addBinding(params, 'radius', {
-	min: 0,
-	max: 1,
-	step: 0.001,
-}).on('change', updateBloom);
-bP.addBinding(params, 'threshold', {
-	min: 0,
-	max: 1,
-	step: 0.001,
-}).on('change', updateBloom);
-
-pane.addBinding(uniforms.uFrequency, 'value', {
-	label: 'Frequency',
-	max: 2,
-	min: 0,
-	step: 0.001,
-});
-pane.addBinding(uniforms.uProgress, 'value', {
-	label: 'Progress',
-	max: 1,
-	min: -1.08,
-	step: 0.001,
-});
+const sunP = pane.addFolder({ title: 'Sun' });
+sunP
+	.addBinding(sunSpherical, 'theta', {
+		max: Math.PI,
+		min: -Math.PI,
+		step: 0.001,
+	})
+	.on('change', updateSun);
+sunP
+	.addBinding(sunSpherical, 'phi', {
+		max: Math.PI,
+		min: 0,
+		step: 0.001,
+	})
+	.on('change', updateSun);
 
 /**
  * Event
  */
-
-function composerRender() {
-	scene.traverse(darkenMaterial);
-	scene.background = null;
-
-	bloomComposer.render();
-
-	scene.traverse(restoreMaterial);
-	scene.background = environment;
-
-	composer.render();
-}
 
 function render() {
 	// Time
 	const delta = clock.getDelta();
 
 	// Render
-	composerRender();
+	renderer.render(scene, camera);
 
 	// Update
 	controls.update(delta);
+	controls2.update();
+	earth.rotation.y += 0.001;
 
 	// Animation
 	requestAnimationFrame(render);
@@ -284,23 +205,8 @@ function resize() {
 	sizes.height = window.innerHeight;
 
 	renderer.setSize(sizes.width, sizes.height);
-	composer.setSize(sizes.width, sizes.height);
 
 	camera.aspect = sizes.width / sizes.height;
 	camera.updateProjectionMatrix();
 }
 window.addEventListener('resize', resize);
-
-function darkenMaterial(obj: Object3D) {
-	if (obj instanceof Mesh && layer.test(obj.layers) === false) {
-		materials[obj.uuid] = obj.material;
-		obj.material = darkMaterial;
-	}
-}
-
-function restoreMaterial(obj: Object3D) {
-	if (obj instanceof Mesh && materials[obj.uuid]) {
-		obj.material = materials[obj.uuid];
-		delete materials[obj.uuid];
-	}
-}
