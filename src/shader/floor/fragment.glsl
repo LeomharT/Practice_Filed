99f8c3fa79d2f9cfa7b7b992c35cd98de36bdaf8
LeomharT <leo.liao.lzy@gmail.com>
2025-12-05 21:57:08 +0800

    feat: clean code

--- src/shader/floor/fragment.glsl
+++ src/shader/floor/fragment.glsl
@@ -6,23 +6,6 @@ uniform vec3 uWaveColor2;
 
 void main(){
     vec3 color  = vec3(1.0);
-    vec2 center = vec2(0.5);
-    vec2 uv     = vUv;
-
-    float distanceToCenter = distance(center, uv);
-
-    if(distanceToCenter > 0.5) discard;
-
-    distanceToCenter  = smoothstep(0.0, 0.95, distanceToCenter);
-    distanceToCenter *= 8.0;
-    distanceToCenter -= uTime * 0.5;
-    distanceToCenter  = fract(distanceToCenter);
-
-    color = mix(
-        uWaveColor2,
-        uWaveColor,
-        distanceToCenter
-    );
 
     gl_FragColor = vec4(color, 1.0);
 
