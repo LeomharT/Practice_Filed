51fabcca711553fdc722382829af3e2f3782dffb
LeomharT <leo.liao.lzy@gmail.com>
2025-12-05 22:12:33 +0800

    feat: displacement position

--- src/shader/floor/vertex.glsl
+++ src/shader/floor/vertex.glsl
@@ -1,17 +1,20 @@
+#define GRAVITY 1.45
+
 varying vec2 vUv;
 
 void main(){
-    float distanceToCenter = distance(vec2(0.5), uv);
+    float distanceToCenter     = distance(vec2(0.5), uv);
+    vec3  displacementPosition = position;
 
-    vec4 modelPosition    = modelMatrix * vec4(position, 1.0);
-         modelPosition.y += pow(distanceToCenter, 1.5);
-    
-    if(distanceToCenter >= 0.3) {
-        modelPosition.y = pow(0.3, 1.5);
+    displacementPosition.z = pow(distanceToCenter, GRAVITY);
+
+    if(distanceToCenter > 0.45) {
+        displacementPosition.z = pow(0.45, GRAVITY);
     }
 
-    vec4  viewPosition       = viewMatrix * modelPosition;
-    vec4  projectionPosition = projectionMatrix * viewPosition;
+    vec4 modelPosition      = modelMatrix * vec4(displacementPosition, 1.0);
+    vec4 viewPosition       = viewMatrix * modelPosition;
+    vec4 projectionPosition = projectionMatrix * viewPosition;
 
     gl_Position = projectionPosition;
 
