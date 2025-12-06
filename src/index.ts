c400a439204bc668f04c75b07bda7bdababf1a81
LeomharT <leo.liao.lzy@gmail.com>
2025-12-06 22:44:53 +0800

    feat: line grid

--- src/index.ts
+++ src/index.ts
@@ -118,6 +118,7 @@ const uniforms = {
   uTime: new Uniform(0.0),
   uWaveColor: new Uniform(new Color('#722ed1')),
   uWaveColor2: new Uniform(new Color('#eb2f96')),
+  uLineColor: new Uniform(new Color('#ffe58f')),
 };
 
 const planeGeometry = new PlaneGeometry(2, 2, 64, 64);
--- src/shader/floor/fragment.glsl
+++ src/shader/floor/fragment.glsl
@@ -5,12 +5,14 @@ varying vec2 vUv;
 uniform float uTime;
 uniform vec3 uWaveColor;
 uniform vec3 uWaveColor2;
+uniform vec3 uLineColor;
 
 void main(){
     vec3  color            = vec3(1.0);
     vec2  uv               = vUv;
     vec2  center           = vec2(0.5);
     float distanceToCenter = distance(uv, center);
+    float lineWidth        = 0.01;
 
     color = uWaveColor;
 
@@ -23,6 +25,29 @@ void main(){
 
     if(distanceToCenter < 0.5 && distanceToCenter > 0.48) {
         color = uWaveColor2;
+    }
+
+    float gap = 1.0 / 5.0;
+
+    uv *= 5.0;
+    uv += uTime * 0.2;
+    uv  = fract(uv);
+ 
+    for(float i = 0.0; i < 5.0; i += 1.0) {
+        if(i == 0.0) {
+            lineWidth = 0.03;
+        }else {
+            lineWidth = 0.01;
+        }
+
+        if(uv.x > gap * i && uv.x < lineWidth + gap * i) {
+            color = uLineColor;
+            alpha = 1.0;
+        }
+        if(uv.y > gap * i && uv.y < lineWidth + gap * i) {
+            color = uLineColor;
+            alpha = 1.0;
+        }
     }
 
     gl_FragColor = vec4(color, alpha);
