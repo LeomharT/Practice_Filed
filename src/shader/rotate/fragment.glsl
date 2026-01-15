c6851c7ca460138abf48f25b8fdbcd2593380b79
LeomharT <leo.liao.lzy@gmail.com>
2026-01-15 10:08:36 +0800

    fix: math error

--- src/shader/rotate/fragment.glsl
+++ src/shader/rotate/fragment.glsl
@@ -7,11 +7,7 @@ vec2 rotate(vec2 p, float angle) {
         cos(angle), -sin(angle),
         sin(angle), cos(angle)
     );
-
-    float x = p.x * m[0][0] - p.y * m[0][1];
-    float y = p.x * m[1][0] + p.y * m[1][1];
-
-    return vec2(x, y);
+    return m * p;
 }
 
 
@@ -20,7 +16,7 @@ void main(){
     vec3 color = vec3(1.0);
 
     uv -= vec2(0.5);
-    uv = rotate(uv, uRotateDeg * 3.14);
+    uv = rotate(uv, uRotateDeg);
     uv += vec2(0.5);
 
     vec3 color1 = vec3(0.125, 0.758, 0.015);
