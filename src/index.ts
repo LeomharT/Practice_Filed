d9a98d2f1a05ba9db74fe91cbb5c5001148936f4
LeomharT <leo.liao.lzy@gmail.com>
2026-02-03 10:29:20 +0800

    feat: practice at border

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,81 @@
- 
+import * as EssentialsPlugin from '@tweakpane/plugin-essentials';
+import {
+  AxesHelper,
+  Color,
+  PerspectiveCamera,
+  PlaneGeometry,
+  Scene,
+  ShaderMaterial,
+  WebGLRenderer,
+} from 'three';
+import { OrbitControls } from 'three/examples/jsm/Addons.js';
+import { Pane } from 'tweakpane';
+import './style.css';
+
+const size = {
+  width: window.innerWidth,
+  height: window.innerHeight,
+  pixelRatio: Math.min(2.0, window.devicePixelRatio),
+};
+
+const el = document.querySelector('#root');
+
+const renderer = new WebGLRenderer({
+  alpha: true,
+  antialias: true,
+});
+renderer.setSize(size.width, size.height);
+renderer.setPixelRatio(size.pixelRatio);
+el?.append(renderer.domElement);
+
+const scene = new Scene();
+scene.background = new Color('#1e1e1e');
+
+const camera = new PerspectiveCamera(75, size.width / size.height, 0.1, 1000);
+camera.position.set(3, 3, 3);
+camera.lookAt(scene.position);
+
+const controls = new OrbitControls(camera, renderer.domElement);
+controls.enableDamping = true;
+
+/** World */
+const floorGeometry = new PlaneGeometry(3, 3, 16, 16);
+const floorMaterial = new ShaderMaterial();
+
+/** Helper */
+const axesHelper = new AxesHelper();
+scene.add(axesHelper);
+
+/** Pane */
+const pane = new Pane({ title: 'Debug Param' });
+pane.element.parentElement!.style.width = '380px';
+pane.registerPlugin(EssentialsPlugin);
+
+const fpsGraph: any = pane.addBlade({
+  view: 'fpsgraph',
+  label: undefined,
+  rows: 4,
+});
+
+function render() {
+  fpsGraph.begin();
+  // Update
+  controls.update();
+  // Render
+  renderer.render(scene, camera);
+  // Animation
+  requestAnimationFrame(render);
+  fpsGraph.end();
+}
+render();
+
+function resize() {
+  size.width = window.innerWidth;
+  size.height = window.innerHeight;
+
+  renderer.setSize(size.width, size.height);
+
+  camera.aspect = size.width / size.height;
+  camera.updateProjectionMatrix();
+}
+window.addEventListener('resize', resize);
