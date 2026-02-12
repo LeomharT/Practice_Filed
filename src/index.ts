be02cb073c77a7560376490abebae9e45d3adb41
LeomharT <leo.liao.lzy@gmail.com>
2026-02-12 10:25:52 +0800

    feat: clean code

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,1 @@
-import { ShaderChunk } from 'three';
-import { Experience } from './Experience';
-import simplex3DNoise from './shader/include/simplex3DNoise.glsl?raw';
-import './style.css';
-
-(ShaderChunk as any)['simplex3DNoise'] = simplex3DNoise;
-
-const root = document.querySelector('#root');
-
-const el = document.createElement('div');
-el.classList.add('viewer');
-root?.append(el);
-
-const experience = Experience.instance;
-el.append(experience.canvas);
+ 
