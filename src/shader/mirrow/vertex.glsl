2df9ad78ebfce6c9e1c5617ae25f263bbbe567e0
LeomharT <leo.liao.lzy@gmail.com>
2026-02-15 11:59:39 +0800

    feat: shadwo?

--- src/shader/mirrow/vertex.glsl
+++ src/shader/mirrow/vertex.glsl
@@ -1,11 +1,10 @@
 void main(){
     vec3 pos = position;
 
-    vec4 modelPosition    = modelMatrix * vec4(0.0, 0.0, 0.0, 1.0);
-    vec4 viewPosition     = viewMatrix * modelPosition;
-         viewPosition.xy += pos.xy;
+    vec4 modelViewPosition = modelViewMatrix * vec4(vec3(0.0), 1.0);
+    modelViewPosition.xyz += pos.xyz;
 
-    vec4 projectionPosition = projectionMatrix * viewPosition;
+    vec4 projectionPosition = projectionMatrix * modelViewPosition;
 
     gl_Position = projectionPosition;
 }
