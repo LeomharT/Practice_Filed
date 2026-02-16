eadb8e5de569232d886dc9f20854fa4d7f51cf1d
LeomharT <leo.liao.lzy@gmail.com>
2026-02-16 21:38:11 +0800

    feat: clean

--- src/index.ts
+++ src/index.ts
@@ -1,39 +1,15 @@
 import {
-  BoxGeometry,
+  AxesHelper,
   Color,
   Layers,
-  Material,
-  Mesh,
-  MeshBasicMaterial,
-  Object3D,
   PerspectiveCamera,
-  PlaneGeometry,
   Scene,
   ShaderChunk,
-  ShaderMaterial,
-  SphereGeometry,
-  Uniform,
-  Vector2,
-  Vector3,
   WebGLRenderer,
 } from 'three';
-import {
-  EffectComposer,
-  OrbitControls,
-  OutputPass,
-  Reflector,
-  RenderPass,
-  ShaderPass,
-  UnrealBloomPass,
-} from 'three/examples/jsm/Addons.js';
+import { OrbitControls } from 'three/examples/jsm/Addons.js';
 import { Pane } from 'tweakpane';
-import ballFragmentShader from './shader/ball/fragment.glsl?raw';
-import ballVertexShader from './shader/ball/vertex.glsl?raw';
-import bloomFragmentShader from './shader/bloom/fragment.glsl?raw';
-import bloomVertexShader from './shader/bloom/vertex.glsl?raw';
 import simplex3DNoise from './shader/include/simplex3DNoise.glsl?raw';
-import mirrowFragmentShader from './shader/mirrow/fragment.glsl?raw';
-import mirrowVertexShader from './shader/mirrow/vertex.glsl?raw';
 import './style.css';
 
 (ShaderChunk as any)['simplex3DNoise'] = simplex3DNoise;
@@ -52,8 +28,6 @@ const layers = {
 const layer = new Layers();
 layer.set(layers.bloom);
 const background = new Color('#1e1e1e');
-const darkMaterial = new MeshBasicMaterial({ color: '#000' });
-const materials: Record<string, Material> = {};
 
 const renderer = new WebGLRenderer({
   alpha: true,
@@ -74,89 +48,16 @@ camera.layers.enable(layers.bloom);
 const controls = new OrbitControls(camera, renderer.domElement);
 controls.enableDamping = true;
 
-const renderScene = new RenderPass(scene, camera);
-const outputPass = new OutputPass();
-
-const bloomPass = new UnrealBloomPass(
-  new Vector2(size.width, size.height),
-  0.5,
-  0.5,
-  0.99,
-);
-
-const bloomComposer = new EffectComposer(renderer);
-bloomComposer.renderToScreen = false;
-bloomComposer.addPass(renderScene);
-bloomComposer.addPass(bloomPass);
-
-const mixPass = new ShaderPass(
-  new ShaderMaterial({
-    uniforms: {
-      uDiffuseColor: new Uniform(null),
-      uBloomTexture: new Uniform(bloomComposer.renderTarget2.texture),
-    },
-    vertexShader: bloomVertexShader,
-    fragmentShader: bloomFragmentShader,
-  }),
-  'uDiffuseColor',
-);
-
-const composer = new EffectComposer(renderer);
-composer.addPass(renderScene);
-composer.addPass(mixPass);
-composer.addPass(outputPass);
-
 /**
  * World
  */
-const uniforms = {
-  uFrequency: new Uniform(2.0),
-  uProgress: new Uniform(0.0),
-  uColor: new Uniform(new Color('#531dab')),
-  uEdgeColor: new Uniform(new Color('#c41d7f')),
-  uDirection: new Uniform(new Vector3()),
-};
 
-const sphereGeometry = new SphereGeometry(1, 64, 64);
-const sphereMaterial = new ShaderMaterial({
-  uniforms,
-  transparent: true,
-  vertexShader: ballVertexShader,
-  fragmentShader: ballFragmentShader,
-});
-const ball = new Mesh(sphereGeometry, sphereMaterial);
-ball.layers.enable(layers.bloom);
-scene.add(ball);
+/**
+ * Helper
+ */
 
-const lightCube = new Mesh(
-  new BoxGeometry(0.5, 0.5, 0.5),
-  new MeshBasicMaterial({ color: new Color(1, 1, 1) }),
-);
-scene.add(lightCube);
-lightCube.position.x = 2;
-lightCube.layers.enable(layers.bloom);
-uniforms.uDirection.value.copy(lightCube.position.clone().normalize());
-
-const floorGeometry = new PlaneGeometry(5, 5, 64, 64);
-const floorReflector = new Reflector(floorGeometry, {
-  textureWidth: size.width,
-  textureHeight: size.height,
-  clipBias: 0.003,
-});
-floorReflector.layers.enable(layers.bloom);
-floorReflector.rotation.x = -Math.PI / 2;
-floorReflector.position.y = -1;
-scene.add(floorReflector);
-
-const mirrowGeometry = new PlaneGeometry(1, 1, 16, 16);
-const mirrowMaterial = new ShaderMaterial({
-  vertexShader: mirrowVertexShader,
-  fragmentShader: mirrowFragmentShader,
-});
-
-const mirrow = new Mesh(mirrowGeometry, mirrowMaterial);
-mirrow.position.set(1, 1, 1);
-scene.add(mirrow);
+const axesHelper = new AxesHelper();
+scene.add(axesHelper);
 
 /**
  * Pane
@@ -164,39 +65,16 @@ scene.add(mirrow);
 
 const pane = new Pane({ title: 'Debug' });
 pane.element.parentElement!.style.width = '380px';
-pane.addBinding(uniforms.uFrequency, 'value', {
-  label: 'Frequency',
-  min: 1.0,
-  max: 10.0,
-  step: 0.1,
-});
-pane.addBinding(uniforms.uProgress, 'value', {
-  label: 'Progress',
-  min: 0.0,
-  max: 1.0,
-  step: 0.001,
-});
 
 /**
  * Event
  */
 
-function renderBloom() {
-  scene.traverse(darkenMatrials);
-  scene.background = null;
-  mirrow.visible = false;
-  bloomComposer.render();
-  mirrow.visible = true;
-  scene.background = background;
-  scene.traverse(restoreMatrial);
-}
-
 function render() {
   // Update
   controls.update();
   // Render
-  renderBloom();
-  composer.render();
+  renderer.render(scene, camera);
   // Animation
   requestAnimationFrame(render);
 }
@@ -212,16 +90,3 @@ function resize() {
   camera.updateProjectionMatrix();
 }
 window.addEventListener('resize', resize);
-
-function darkenMatrials(obj: Object3D) {
-  if (obj instanceof Mesh && obj.layers.test(layer) === false) {
-    materials[obj.uuid] = obj.material;
-    obj.material = darkMaterial;
-  }
-}
-function restoreMatrial(obj: Object3D) {
-  if (obj instanceof Mesh && materials[obj.uuid]) {
-    obj.material = materials[obj.uuid];
-    delete materials[obj.uuid];
-  }
-}
--- src/shader/mirrow/fragment.glsl
+++
@@ -1,5 +0,0 @@
-void main(){
-    vec3 color = vec3(0.7821, 0.1923, 0.1235);
-
-    gl_FragColor = vec4(color, 1.0);   
-}
