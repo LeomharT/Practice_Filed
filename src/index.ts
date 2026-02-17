1f530c4f18df3f549790aa30b73471503f35c5ba
LeomharT <leo.liao.lzy@gmail.com>
2026-02-17 08:00:00 +0800

    feat: create instance start

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,21 @@
 import {
   AxesHelper,
   Color,
+  InstancedMesh,
   Layers,
+  Object3D,
   PerspectiveCamera,
+  PlaneGeometry,
   Scene,
   ShaderChunk,
+  ShaderMaterial,
   WebGLRenderer,
 } from 'three';
 import { OrbitControls } from 'three/examples/jsm/Addons.js';
 import { Pane } from 'tweakpane';
 import simplex3DNoise from './shader/include/simplex3DNoise.glsl?raw';
+import starFragmentShader from './shader/start/fragment.glsl?raw';
+import starVertexShader from './shader/start/vertex.glsl?raw';
 import './style.css';
 
 (ShaderChunk as any)['simplex3DNoise'] = simplex3DNoise;
@@ -41,7 +47,7 @@ const scene = new Scene();
 scene.background = background;
 
 const camera = new PerspectiveCamera(75, size.width / size.height, 0.1, 1000);
-camera.position.set(2, 3, 2);
+camera.position.set(0, 0, 2);
 camera.lookAt(scene.position);
 camera.layers.enable(layers.bloom);
 
@@ -51,6 +57,26 @@ controls.enableDamping = true;
 /**
  * World
  */
+const params = {
+  count: 30,
+};
+
+const startGeometry = new PlaneGeometry(1, 1, 16, 16);
+const startMaterial = new ShaderMaterial({
+  vertexShader: starVertexShader,
+  fragmentShader: starFragmentShader,
+});
+const starts = new InstancedMesh(startGeometry, startMaterial, params.count);
+
+const obj = new Object3D();
+
+for (let i = 0; i < params.count; i++) {
+  obj.position.set(i / 10, Math.random() * 10, Math.random() * 5);
+  obj.updateMatrix();
+
+  starts.setMatrixAt(i, obj.matrix);
+}
+scene.add(starts);
 
 /**
  * Helper
