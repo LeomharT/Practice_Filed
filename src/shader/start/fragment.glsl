bfc9044aebf533a96609356e73de068b4a6caedc
LeomharT <leo.liao.lzy@gmail.com>
2026-02-17 14:05:59 +0800

    feat: color

--- src/shader/start/fragment.glsl
+++ src/shader/start/fragment.glsl
@@ -1,5 +1,10 @@
+varying vec3 vPosition;
+
 void main(){
-    vec3 color = vec3(1.0);
+    vec3 color = vec3(vPosition + 0.5);
 
     gl_FragColor = vec4(color, 1.0);
+    
+    #include <tonemapping_fragment>
+    #include <colorspace_fragment>
 }
--- src/shader/start/vertex.glsl
+++ src/shader/start/vertex.glsl
@@ -1,7 +1,11 @@
+varying vec3 vPosition;
+
 void main(){
     vec4 modelPosition      = instanceMatrix * vec4(position, 1.0);
     vec4 viewPosition       = viewMatrix * modelPosition;
     vec4 projectionPosition = projectionMatrix * viewPosition;
 
     gl_Position = projectionPosition;
+
+    vPosition = modelPosition.xyz;
 }
