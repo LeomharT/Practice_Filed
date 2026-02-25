1b17da8a0d13f72628bf8324848299bcfb40c0e3
LeomharT <leo.liao.lzy@gmail.com>
2026-02-25 15:59:49 +0800

    feat: use variable

--- src/shader/effect/fragment.glsl
+++ src/shader/effect/fragment.glsl
@@ -6,9 +6,11 @@ void main(){
     vec2 uv    = vUv;
     vec3 color = vec3(0.0);
 
-    float r = texture2D(uDiffuse, uv - 0.001).r;
+    float offset = 0.0015;
+
+    float r = texture2D(uDiffuse, uv - offset).r;
     float g = texture2D(uDiffuse, uv).g;
-    float b = texture2D(uDiffuse, uv + 0.001).b;
+    float b = texture2D(uDiffuse, uv + offset).b;
 
     color = vec3(r, g, b);
 
