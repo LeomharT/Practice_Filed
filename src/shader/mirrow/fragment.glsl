dfc933e9f836182ec15fa7911db9832d66ae86bb
LeomharT <leo.liao.lzy@gmail.com>
2026-03-06 20:15:19 +0800

    feat: color

--- src/shader/mirrow/fragment.glsl
+++ src/shader/mirrow/fragment.glsl
@@ -7,10 +7,15 @@ void main() {
   vec2 uv = vUv;
   vec3 color = vec3(1.0);
   vec3 ndc = vNDC;
+  vec2 center = vec2(0.5);
+
+  float edge = length(uv - center);
+
+  if (edge > 0.5) discard;
 
   vec4 diffuseColor = texture2D(uDiffuseTexture, ndc.xy);
   color = diffuseColor.rgb;
-  //   color += vec3(0.125, 0.345, 0.012);
+  color += vec3(0.125, 0.345, 0.012);
 
   gl_FragColor = vec4(color, 1.0);
 
