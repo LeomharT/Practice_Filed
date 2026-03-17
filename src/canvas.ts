a3521e7adca5e1e4cc6e6d3f2f054b3a692fa8f9
LeomharT <leo.liao.lzy@gmail.com>
2026-03-17 15:48:10 +0800

    feat: fllow cursor

--- src/canvas.ts
+++ src/canvas.ts
@@ -1,99 +1,83 @@
 import { Colors } from '@blueprintjs/colors';
-import {
-  AxesHelper,
-  Color,
-  Mesh,
-  PerspectiveCamera,
-  PlaneGeometry,
-  Scene,
-  ShaderChunk,
-  ShaderMaterial,
-  WebGLRenderer,
-} from 'three';
-import {
-  EffectComposer,
-  GLTFLoader,
-  OrbitControls,
-  OutputPass,
-  RenderPass,
-} from 'three/examples/jsm/Addons.js';
-import simplex4DNoise from './shader/include/simplex4DNoise.glsl?raw';
+import { MathUtils } from 'three';
 import './style.css';
-
-(ShaderChunk as any)['simplex4DNoise'] = simplex4DNoise;
 
 const size = {
   width: window.innerWidth,
   height: window.innerHeight,
-  pixelRatio: Math.min(2.0, window.devicePixelRatio),
 };
 
 const el = document.querySelector('#root');
 
-const background = new Color(Colors.BLACK);
+const canvas = document.createElement('canvas');
+canvas.width = size.width;
+canvas.height = size.height;
+canvas.style.width = size.width + 'px';
+canvas.style.height = size.height + 'px';
+el?.append(canvas);
 
-const gltfLoader = new GLTFLoader();
+const ctx = canvas.getContext('2d') as CanvasRenderingContext2D;
 
-const renderer = new WebGLRenderer({
-  antialias: true,
-  alpha: true,
-});
-renderer.setSize(size.width, size.height);
-renderer.setPixelRatio(size.pixelRatio);
-el?.append(renderer.domElement);
+const image = new Image();
+image.src = '/arrow.png';
 
-const scene = new Scene();
-scene.background = background;
+const positions = Array.from({ length: 350 }, () => ({
+  x: MathUtils.randFloat(0, size.width),
+  y: MathUtils.randFloat(0, size.height),
+  length: MathUtils.randFloat(50, 200),
+}));
 
-const camera = new PerspectiveCamera(75, size.width / size.height, 0.1, 1000);
-camera.position.set(3, 3, 3);
-camera.lookAt(scene.position);
+const cursor = {
+  x: 0,
+  y: 0,
+};
 
-const controls = new OrbitControls(camera, renderer.domElement);
-controls.enableDamping = true;
+function clean() {
+  ctx.save();
+  ctx.fillStyle = Colors.GRAY1;
+  ctx.fillRect(0, 0, size.width, size.height);
+  ctx.restore();
+}
 
-const renderScene = new RenderPass(scene, camera);
+function drawArrows() {
+  ctx.save();
 
-const outputPass = new OutputPass();
+  for (const p of positions) {
+    ctx.save();
+    ctx.translate(p.x, p.y);
+    let angle = Math.atan2(p.y - cursor.y, p.x - cursor.x);
+    angle += Math.PI;
+    ctx.rotate(angle);
+    ctx.drawImage(image, -p.length / 2, -50 / 2, p.length, 50);
+    ctx.restore();
+  }
 
-const composer = new EffectComposer(renderer);
-composer.addPass(renderScene);
-composer.addPass(outputPass);
+  ctx.restore();
+}
 
-const grassBaseGeometry = new PlaneGeometry(0.12, 1, 1, 16);
-grassBaseGeometry.translate(0, 0.5, 0);
-const grassMaterial = new ShaderMaterial({
-  wireframe: true,
-});
+function drawCursor() {
+  ctx.save();
 
-const grassBlead = new Mesh(grassBaseGeometry, grassMaterial);
-scene.add(grassBlead);
+  ctx.beginPath();
+  ctx.fillStyle = Colors.BLUE5;
+  ctx.arc(cursor.x, cursor.y, 20, 0, Math.PI * 2);
+  ctx.fill();
 
-/**
- * Helpers
- */
-
-const axesHelper = new AxesHelper(3);
-scene.add(axesHelper);
+  ctx.restore();
+}
 
 function render() {
-  // Update
-  controls.update();
-  // Redner
-  composer.render();
+  // Clean
+  clean();
+  // Render
+  drawArrows();
+  drawCursor();
   // Animation
   requestAnimationFrame(render);
 }
-
 render();
 
-function resize() {
-  size.width = window.innerWidth;
-  size.height = window.innerHeight;
-
-  renderer.setSize(size.width, size.height);
-
-  camera.aspect = size.width / size.height;
-  camera.updateProjectionMatrix();
-}
-window.addEventListener('resize', resize);
+canvas.addEventListener('pointermove', (e) => {
+  cursor.x = e.clientX;
+  cursor.y = e.clientY;
+});
