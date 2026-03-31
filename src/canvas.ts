43ce11f2533a4cdcd602be6a0c1d2e1465bd50cd
LeomharT <leo.liao.lzy@gmail.com>
2026-03-31 09:38:41 +0800

    feat: practice every day

--- src/canvas.ts
+++ src/canvas.ts
@@ -1,17 +1,26 @@
 import { Colors } from '@blueprintjs/colors';
 import {
+  AxesHelper,
   Color,
+  DirectionalLight,
   IcosahedronGeometry,
   Mesh,
-  MeshBasicMaterial,
+  MeshStandardMaterial,
+  PCFShadowMap,
   PerspectiveCamera,
   PlaneGeometry,
-  Raycaster,
   Scene,
-  Vector2,
+  ShaderChunk,
+  ShaderMaterial,
   WebGLRenderer,
 } from 'three';
+import { OrbitControls } from 'three/examples/jsm/Addons.js';
+import simplex4DNoise from './shader/include/simplex4DNoise.glsl?raw';
+import testFragmentShader from './shader/test/fragment.glsl?raw';
+import testVertexShader from './shader/test/vertex.glsl?raw';
 import './style.css';
+
+(ShaderChunk as any)['simplex4DNoise'] = simplex4DNoise;
 
 const size = {
   width: window.innerWidth,
@@ -27,77 +36,67 @@ const renderer = new WebGLRenderer({
 });
 renderer.setSize(size.width, size.height);
 renderer.setPixelRatio(size.pixelRatio);
+renderer.shadowMap.enabled = true;
+renderer.shadowMap.type = PCFShadowMap;
 el?.append(renderer.domElement);
 
 const scene = new Scene();
 scene.background = new Color(Colors.BLACK);
 
 const camera = new PerspectiveCamera(75, size.width / size.height, 0.1, 1000);
-camera.position.set(0, 0, 3);
+camera.position.set(0, 5, 5);
 camera.lookAt(scene.position);
 
-const raycaster = new Raycaster();
+const controls = new OrbitControls(camera, renderer.domElement);
+controls.enableDamping = true;
 
 /**
  * World
  */
 
-const planeGeometry = new PlaneGeometry(4, 4, 16, 16);
-const planeMaterial = new MeshBasicMaterial({
-  color: new Color(Colors.BLUE3),
-  wireframe: true,
+const uniforms = {};
+
+const ballGeometry = new IcosahedronGeometry(2, 50);
+const ballMaterial = new ShaderMaterial({
+  uniforms,
+  vertexShader: testVertexShader,
+  fragmentShader: testFragmentShader,
 });
+const ball = new Mesh(ballGeometry, ballMaterial);
+ball.castShadow = true;
+scene.add(ball);
+
+const planeGeometry = new PlaneGeometry(10, 10, 32, 32);
+const planeMaterial = new MeshStandardMaterial({});
 const plane = new Mesh(planeGeometry, planeMaterial);
+plane.position.set(-3, -3, 0);
+plane.lookAt(ball.position);
+plane.receiveShadow = true;
 scene.add(plane);
 
-const pointGeometry = new IcosahedronGeometry(0.1, 3);
-const pointMaterial = new MeshBasicMaterial({
-  color: Colors.GOLD5,
-});
-const point = new Mesh(pointGeometry, pointMaterial);
-scene.add(point);
+const directionLight = new DirectionalLight('#fff', 2);
+directionLight.position.set(3, 2, 0);
+directionLight.castShadow = true;
+scene.add(directionLight);
 
-const cursor = new Vector2();
-
-let translateY = 0;
-let accelerationY = 0;
-
-let translateX = 0;
-let accelerationX = 0;
-
-function intersectObject() {
-  raycaster.setFromCamera(cursor, camera);
-  const intersect = raycaster.intersectObject(plane);
-
-  if (intersect.length) {
-    let targetY = intersect[0].point.y - translateY;
-    accelerationY += targetY;
-    accelerationY *= 0.95;
-    translateY += accelerationY * 0.01;
-
-    const targetX = intersect[0].point.x - translateX;
-    accelerationX += targetX;
-    accelerationX *= 0.95;
-    translateX += accelerationX * 0.01;
-
-    point.position.x = translateX;
-    point.position.y = translateY;
-  }
-}
+const axesHelper = new AxesHelper(5);
+scene.add(axesHelper);
 
 function render() {
-  intersectObject();
+  controls.update();
+
   renderer.render(scene, camera);
+
   requestAnimationFrame(render);
 }
 render();
 
-window.addEventListener('pointermove', (e) => {
-  const x = (e.clientX / size.width) * 2 - 1;
-  const y = -(e.clientY / size.height) * 2 + 1;
+window.addEventListener('resize', () => {
+  size.width = window.innerWidth;
+  size.height = window.innerHeight;
 
-  cursor.x = x;
-  cursor.y = y;
+  renderer.setSize(size.width, size.height);
 
-  raycaster.setFromCamera(cursor, camera);
+  camera.aspect = size.width / size.height;
+  camera.updateProjectionMatrix();
 });
