9581e120647bee5b47a50e379d93ce7cea08e727
LeomharT <leo.liao.lzy@gmail.com>
2026-04-07 11:31:51 +0800

    feat: max polar angle

--- src/canvas.ts
+++ src/canvas.ts
@@ -1,90 +1,70 @@
 import { Colors } from '@blueprintjs/colors';
-import { MathUtils } from 'three';
+import {
+  BoxGeometry,
+  Color,
+  Mesh,
+  MeshBasicMaterial,
+  PerspectiveCamera,
+  Scene,
+  WebGLRenderer,
+} from 'three';
+import { OrbitControls } from 'three/examples/jsm/Addons.js';
 import './style.css';
 
 const size = {
   width: window.innerWidth,
   height: window.innerHeight,
+  pixelRatio: Math.min(2, window.devicePixelRatio),
 };
 
 const el = document.querySelector('#root');
 
-const canvas = document.createElement('canvas');
-canvas.width = size.width;
-canvas.height = size.height;
-canvas.style.width = size.width + 'px';
-canvas.style.height = size.height + 'px';
-el?.append(canvas);
-
-const ctx = canvas.getContext('2d') as CanvasRenderingContext2D;
-
-const LENGTH = 500;
-
-function clean() {
-  ctx.save();
-  ctx.fillStyle = Colors.BLACK;
-  ctx.fillRect(0, 0, size.width, size.height);
-  ctx.restore();
-}
-clean();
-
-function line(angle: number = 0) {
-  ctx.save();
-
-  const w = size.width / 2;
-  const h = size.height / 2;
-
-  ctx.lineWidth = 20;
-  ctx.lineCap = 'round';
-  ctx.strokeStyle = Colors.BLUE5;
-  ctx.beginPath();
-  ctx.translate(w, h);
-  ctx.rotate(angle);
-  ctx.moveTo(-LENGTH / 2, 0);
-  ctx.lineTo(LENGTH / 2, 0);
-  ctx.stroke();
-
-  ctx.restore();
-}
-
-line();
-
-let enabled = false;
-let angle = 0;
-let targetAngle = 0;
-
-const center = {
-  x: size.width / 2,
-  y: size.height / 2,
-};
-
-canvas.addEventListener('pointerdown', () => (enabled = true));
-canvas.addEventListener('pointerup', () => (enabled = false));
-canvas.addEventListener('pointermove', (e) => {
-  if (enabled) {
-    const target = {
-      x: e.clientX,
-      y: e.clientY,
-    };
-
-    targetAngle = Math.atan2(target.y - center.y, target.x - center.x);
-  }
+const renderer = new WebGLRenderer({
+  antialias: true,
+  alpha: true,
 });
+renderer.setSize(size.width, size.height);
+renderer.setPixelRatio(size.pixelRatio);
+el?.append(renderer.domElement);
 
-let prevTime = 0;
-const speed = 5;
+const scene = new Scene();
+scene.background = new Color(Colors.BLACK);
 
-function render(time: number = 0) {
-  const delta = (time - prevTime) / 1000;
-  prevTime = time;
+const camera = new PerspectiveCamera(70, size.width / size.height, 0.1, 1000);
+camera.position.set(0, 0, 3);
+camera.lookAt(scene.position);
 
-  const t = 1.0 - Math.exp(-speed * delta);
+const controls = new OrbitControls(camera, renderer.domElement);
+controls.enableDamping = true;
+controls.maxPolarAngle = Math.PI / 2;
 
-  angle = MathUtils.lerp(angle, targetAngle, t);
+/**
+ * World
+ */
 
-  clean();
-  line(angle);
+const planeGeometry = new BoxGeometry(2, 2, 2, 16, 16, 16);
+const planeMaterial = new MeshBasicMaterial({
+  color: Colors.GOLD4,
+  wireframe: true,
+});
+const plane = new Mesh(planeGeometry, planeMaterial);
+scene.add(plane);
+
+function render() {
+  controls.update();
+
+  renderer.render(scene, camera);
+
   requestAnimationFrame(render);
 }
-
 render();
+
+window.addEventListener('resize', () => {
+  size.width = window.innerWidth;
+  size.height = window.innerHeight;
+
+  renderer.setSize(size.width, size.height);
+
+  camera.aspect = size.width / size.height;
+  camera.updateProjectionMatrix();
+});
