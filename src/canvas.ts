45b055eb7e07357a310fad4e9c1bacc4b8d35426
LeomharT <leo.liao.lzy@gmail.com>
2026-04-14 10:15:53 +0800

    feat: lerp is so cool

--- src/canvas.ts
+++ src/canvas.ts
@@ -2,29 +2,25 @@ import { Colors } from '@blueprintjs/colors';
 import {
   AxesHelper,
   Color,
-  DoubleSide,
+  IcosahedronGeometry,
+  MathUtils,
   Mesh,
+  MeshBasicMaterial,
   PerspectiveCamera,
   PlaneGeometry,
+  Raycaster,
   Scene,
-  ShaderChunk,
-  ShaderMaterial,
-  Uniform,
+  Timer,
+  Vector2,
+  Vector3,
   WebGLRenderer,
 } from 'three';
-import { OrbitControls } from 'three/examples/jsm/Addons.js';
-import { Pane } from 'tweakpane';
-import simplex2DNoise from './shader/include/simplex2DNoise.glsl?raw';
-import fragmentShader from './shader/test/fragment.glsl?raw';
-import vertexShader from './shader/test/vertex.glsl?raw';
 import './style.css';
-
-(ShaderChunk as any)['simplex2DNoise'] = simplex2DNoise;
 
 const size = {
   width: window.innerWidth,
   height: window.innerHeight,
-  pixelRatio: Math.min(2, window.devicePixelRatio),
+  piexlRatio: Math.min(2, window.devicePixelRatio),
 };
 
 const el = document.querySelector('#root');
@@ -34,59 +30,61 @@ const renderer = new WebGLRenderer({
   alpha: true,
 });
 renderer.setSize(size.width, size.height);
-renderer.setPixelRatio(size.pixelRatio);
+renderer.setPixelRatio(size.piexlRatio);
 el?.append(renderer.domElement);
 
 const scene = new Scene();
 scene.background = new Color(Colors.BLACK);
 
-const camera = new PerspectiveCamera(70, size.width / size.height, 0.1, 1000);
-camera.position.set(0, 8, 8);
+const camera = new PerspectiveCamera(75, size.width / size.height, 0.1, 1000);
+camera.position.set(0, 0, 3);
 camera.lookAt(scene.position);
 
-const controls = new OrbitControls(camera, renderer.domElement);
-controls.enableDamping = true;
+const raycaster = new Raycaster();
+
+const timer = new Timer();
 
 /**
  * World
  */
 
-const uniforms = {
-  uTime: new Uniform(1),
-  uColor: new Uniform(new Color(Colors.BLUE4).convertSRGBToLinear()),
-  uWidth: new Uniform(16.0),
-};
+const cursor = new Vector2();
+const pointer = new Vector3();
 
-const planeGeometry = new PlaneGeometry(16, 16, 64, 64);
-const planeMaterial = new ShaderMaterial({
-  uniforms,
-  vertexShader,
-  fragmentShader,
-  side: DoubleSide,
+const planeGeometry = new PlaneGeometry(5, 5, 16, 16);
+const planeMaterial = new MeshBasicMaterial({
+  color: 'lightBlue',
+  wireframe: true,
 });
 const plane = new Mesh(planeGeometry, planeMaterial);
-plane.rotation.x = -Math.PI / 2;
 scene.add(plane);
 
-const axesHelper = new AxesHelper(3);
+const ballGeometry = new IcosahedronGeometry(0.1, 3);
+const ballMaterial = new MeshBasicMaterial();
+const ball = new Mesh(ballGeometry, ballMaterial);
+scene.add(ball);
+
+const axesHelper = new AxesHelper();
 scene.add(axesHelper);
 
-const pane = new Pane({ title: 'Debug Pane' });
-pane
-  .addBinding(uniforms.uColor, 'value', {
-    color: { type: 'float' },
-  })
-  .on('change', (val) => {
-    uniforms.uColor.value = new Color(val.value).convertSRGBToLinear();
-  });
+/**
+ * Events
+ */
+
+const speed = 10;
 
 function render() {
-  uniforms.uTime.value += 0.0016;
+  // Update
+  timer.update();
+  const delta = timer.getDelta();
+  const t = 1.0 - Math.exp(speed * -delta);
 
-  controls.update();
+  ball.position.x = MathUtils.lerp(ball.position.x, pointer.x, t);
+  ball.position.y = MathUtils.lerp(ball.position.y, pointer.y, t);
 
+  // Render
   renderer.render(scene, camera);
-
+  // Animation
   requestAnimationFrame(render);
 }
 render();
@@ -99,4 +97,13 @@ window.addEventListener('resize', () => {
 
   camera.aspect = size.width / size.height;
   camera.updateProjectionMatrix();
+});
+
+window.addEventListener('pointermove', (e) => {
+  cursor.x = (e.clientX / size.width) * 2 - 1.0;
+  cursor.y = -(e.clientY / size.height) * 2 + 1.0;
+
+  raycaster.setFromCamera(cursor, camera);
+  const intersect = raycaster.intersectObject(plane);
+  intersect.length && pointer.copy(intersect[0].point);
 });
