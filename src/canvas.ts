9c3414077114505255f910274af6d49e41570469
LeomharT <leo.liao.lzy@gmail.com>
2026-05-28 19:32:42 +0800

    feat: gogogogo

--- src/canvas.ts
+++ src/canvas.ts
@@ -1,132 +1,112 @@
 import { Colors } from '@blueprintjs/colors';
-import {
-  AxesHelper,
-  Color,
-  DoubleSide,
-  IcosahedronGeometry,
-  Mesh,
-  MeshBasicMaterial,
-  PerspectiveCamera,
-  PlaneGeometry,
-  Scene,
-  ShaderChunk,
-  ShaderMaterial,
-  Spherical,
-  Uniform,
-  Vector3,
-  WebGLRenderer,
-} from 'three';
-import { OrbitControls } from 'three/examples/jsm/Addons.js';
-import { Pane } from 'tweakpane';
-import simplex2DNoise from './shader/include/simplex2DNoise.glsl?raw';
-import fragmentShader from './shader/test/fragment.glsl?raw';
-import vertexShader from './shader/test/vertex.glsl?raw';
+import { fs, vs } from './Monkey123';
 import './style.css';
-
-(ShaderChunk as any)['simplex2DNoise'] = simplex2DNoise;
-
+type Vector2 = {
+  x: number;
+  y: number;
+};
+type Vector3 = Vector2 & {
+  z: number;
+};
 const size = {
   width: window.innerWidth,
   height: window.innerHeight,
-  pixelRatio: Math.min(2, window.devicePixelRatio),
+  aspect: window.innerWidth / window.innerHeight,
 };
-
 const el = document.querySelector('#root');
-
-const renderer = new WebGLRenderer({
-  alpha: true,
-  antialias: true,
-});
-renderer.setSize(size.width, size.height);
-renderer.setPixelRatio(size.pixelRatio);
-el?.append(renderer.domElement);
-
-const scene = new Scene();
-scene.background = new Color(Colors.BLACK);
-
-const camera = new PerspectiveCamera(75, size.width / size.height, 0.1, 1000);
-camera.position.set(2, 2, 2);
-camera.lookAt(scene.position);
-
-const controls = new OrbitControls(camera, renderer.domElement);
-controls.enableDamping = true;
-
-/**
- * World
- */
-
-const uniforms = {
-  uDirection: new Uniform(new Vector3()),
-  uColor: new Uniform(new Color(Colors.GREEN4)),
-};
-
-const sunSpherical = new Spherical(1, Math.PI / 4, 0.5);
-const sunPosition = new Vector3();
-
-const sunGeometry = new IcosahedronGeometry(0.1, 3);
-const sunMaterial = new MeshBasicMaterial({
-  color: 'yellow',
-});
-const sun = new Mesh(sunGeometry, sunMaterial);
-scene.add(sun);
-
-function updateSun() {
-  // Position
-  sunPosition.setFromSpherical(sunSpherical);
-  // Uniform
-  uniforms.uDirection.value.copy(sunPosition);
-  // Sun
-  sun.position.copy(sunPosition.clone().multiplyScalar(3));
+const canvas = document.createElement('canvas');
+canvas.width = size.width;
+canvas.height = size.height;
+canvas.style.width = size.width + 'px';
+canvas.style.height = size.height + 'px';
+el?.append(canvas);
+const ctx = canvas.getContext('2d') as CanvasRenderingContext2D;
+function clear(color: string = Colors.BLACK) {
+  ctx.save();
+  ctx.fillStyle = color;
+  ctx.fillRect(0, 0, size.width, size.height);
+  ctx.restore();
 }
+function point(v: Vector2, color: string = Colors.BLUE1) {
+  ctx.save();
+  const s = 10;
+  ctx.fillStyle = color;
+  ctx.fillRect(v.x - s / 2, v.y - s / 2, s, s);
+  ctx.restore();
+}
+function screen(v: Vector2): Vector2 {
+  const x = ((v.x + 1.0) / 2.0) * size.width;
+  const y = -((v.y - 1.0) / 2.0) * size.height;
+  return {
+    x,
+    y,
+  };
+}
+function project(v: Vector3) {
+  return {
+    x: v.x / v.z,
+    y: (v.y / v.z) * size.aspect,
+  };
+}
+function translateZ(v: Vector3, dz: number) {
+  return {
+    ...v,
+    z: v.z + dz,
+  };
+}
+function line(from: Vector2, to: Vector2) {
+  ctx.save();
+  ctx.beginPath();
+  ctx.strokeStyle = Colors.VIOLET3;
+  ctx.lineWidth = 3;
+  ctx.moveTo(from.x, from.y);
+  ctx.lineTo(to.x, to.y);
+  ctx.stroke();
+  ctx.restore();
+}
+function rotate(v: Vector3, angle: number) {
+  const c = Math.cos(angle);
+  const s = Math.sin(angle);
+  return {
+    ...v,
+    x: v.x * c - v.z * s,
+    z: v.x * s + v.z * c,
+  };
+}
+let prevTime = 0;
+let dz = 3.5;
+let angle = 0;
+function render(time: number = 0) {
+  // Update
+  const dt = (time - prevTime) / 1000;
+  prevTime = time;
+  // dz += dt;
+  angle += dt;
+  clear();
+  for (const v of vs) {
+    point(screen(project(translateZ(rotate({ ...v }, angle), dz))));
+  }
+  for (const f of fs) {
+    for (let i = 0; i < f.length; i++) {
+      const from = vs[f[i]];
+      const to = vs[f[(i + 1) % f.length]];
 
-updateSun();
-
-const planeGeometry = new PlaneGeometry(12, 12, 1024, 1024);
-const planeMaterial = new ShaderMaterial({
-  vertexShader,
-  fragmentShader,
-  uniforms,
-  side: DoubleSide,
-});
-const plane = new Mesh(planeGeometry, planeMaterial);
-plane.rotation.x = -Math.PI / 2;
-scene.add(plane);
-
-const axesHelper = new AxesHelper(2);
-scene.add(axesHelper);
-
-const pane = new Pane({ title: 'Debug' });
-pane
-  .addBinding(sunSpherical, 'phi', {
-    min: 0,
-    max: Math.PI,
-    step: 0.01,
-  })
-  .on('change', updateSun);
-pane
-  .addBinding(sunSpherical, 'theta', {
-    min: -Math.PI,
-    max: Math.PI,
-    step: 0.01,
-  })
-  .on('change', updateSun);
-
-function render() {
-  //Update
-  controls.update();
-  //Render
-  renderer.render(scene, camera);
+      line(
+        screen(project(translateZ(rotate({ ...from }, angle), dz))),
+        screen(project(translateZ(rotate({ ...to }, angle), dz))),
+      );
+    }
+  }
   //Animation
   requestAnimationFrame(render);
 }
 render();
-
 window.addEventListener('resize', () => {
   size.width = window.innerWidth;
   size.height = window.innerHeight;
-
-  renderer.setSize(size.width, size.height);
-
-  camera.aspect = size.width / size.height;
-  camera.updateProjectionMatrix();
+  size.aspect = size.width / size.height;
+  canvas.width = size.width;
+  canvas.height = size.height;
+  canvas.style.width = size.width + 'px';
+  canvas.style.height = size.height + 'px';
 });
