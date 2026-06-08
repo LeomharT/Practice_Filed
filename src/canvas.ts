110888a65c26159e76013af2e5d253701a33c4a7
LeomharT <leo.liao.lzy@gmail.com>
2026-06-08 13:26:52 +0800

    refactor(canvas): simplify the physics demo

--- src/canvas.ts
+++ src/canvas.ts
@@ -1,37 +1,19 @@
-import { Colors } from '@blueprintjs/colors';
 import { ColliderDesc, RigidBodyDesc, World } from '@dimforge/rapier3d';
-import * as EssentialsPlugin from '@tweakpane/plugin-essentials';
 import {
-  AmbientLight,
   AxesHelper,
   BufferAttribute,
   BufferGeometry,
-  ClampToEdgeWrapping,
   Color,
-  DirectionalLight,
-  DodecahedronGeometry,
-  Euler,
   LineBasicMaterial,
   LineSegments,
   Mesh,
   MeshBasicMaterial,
-  MeshPhysicalMaterial,
-  PCFShadowMap,
   PerspectiveCamera,
-  PlaneGeometry,
-  Raycaster,
   Scene,
-  ShadowMaterial,
-  SRGBColorSpace,
-  TextureLoader,
-  Timer,
-  Vector2,
-  Vector3,
+  SphereGeometry,
   WebGLRenderer,
 } from 'three';
-import { ThreePerf } from 'three-perf';
-import { DecalGeometry, OrbitControls, TrackballControls } from 'three/examples/jsm/Addons.js';
-import { Pane } from 'tweakpane';
+import { OrbitControls } from 'three/examples/jsm/Addons.js';
 import './style.css';
 
 const sizes = {
@@ -39,270 +21,112 @@ const sizes = {
   height: window.innerHeight,
   pixelRatio: Math.min(2, window.devicePixelRatio),
 };
+const gravity = { x: 0, y: 0, z: 0 };
 
 const el = document.querySelector('#root');
 
-const textureLoader = new TextureLoader();
+const accents = ['#ff4060', '#ffcc00', '#20ffa0', '#4060ff'];
 
-const threeTexture = textureLoader.load('/three.png');
-threeTexture.colorSpace = SRGBColorSpace;
-threeTexture.wrapS = ClampToEdgeWrapping;
-threeTexture.wrapT = ClampToEdgeWrapping;
-
-const reactTexture = textureLoader.load('/react.png');
-reactTexture.colorSpace = SRGBColorSpace;
-reactTexture.wrapS = ClampToEdgeWrapping;
-reactTexture.wrapT = ClampToEdgeWrapping;
+const shuffle = (accent = 0) => [
+  { color: '#444', roughness: 0.1, metalness: 0.5 },
+  { color: '#444', roughness: 0.1, metalness: 0.5 },
+  { color: '#444', roughness: 0.1, metalness: 0.5 },
+  { color: 'white', roughness: 0.1, metalness: 0.1 },
+  { color: 'white', roughness: 0.1, metalness: 0.1 },
+  { color: 'white', roughness: 0.1, metalness: 0.1 },
+  { color: accents[accent], roughness: 0.1, accent: true },
+  { color: accents[accent], roughness: 0.1, accent: true },
+  { color: accents[accent], roughness: 0.1, accent: true },
+  { color: '#444', roughness: 0.1 },
+  { color: '#444', roughness: 0.3 },
+  { color: '#444', roughness: 0.3 },
+  { color: 'white', roughness: 0.1 },
+  { color: 'white', roughness: 0.2 },
+  { color: 'white', roughness: 0.1 },
+  { color: accents[accent], roughness: 0.1, accent: true, transparent: true, opacity: 0.5 },
+  { color: accents[accent], roughness: 0.3, accent: true },
+  { color: accents[accent], roughness: 0.1, accent: true },
+];
 
 const renderer = new WebGLRenderer({
-  alpha: true,
   antialias: true,
+  alpha: true,
 });
 renderer.setSize(sizes.width, sizes.height);
 renderer.setPixelRatio(sizes.pixelRatio);
 renderer.shadowMap.enabled = true;
-renderer.shadowMap.type = PCFShadowMap;
 el?.append(renderer.domElement);
 
 const scene = new Scene();
 scene.background = new Color('#141622');
 
-const camera = new PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 1000);
-camera.position.set(2, 2, 2);
+const camera = new PerspectiveCamera(17.56, sizes.width / sizes.height, 0.1, 1000);
+camera.position.set(0, 0, 30);
 camera.lookAt(scene.position);
 
-const controls1 = new TrackballControls(camera, renderer.domElement);
-controls1.enabled = true;
-controls1.noRotate = true;
-controls1.noPan = true;
-controls1.noZoom = false;
-
-const controls2 = new OrbitControls(camera, renderer.domElement);
-controls2.enableDamping = true;
-controls2.enablePan = true;
-controls2.enableRotate = true;
-controls2.enableZoom = false;
-
-const timer = new Timer();
-
-const raycaster = new Raycaster();
-const mouse = new Vector2();
-const point = new Vector3();
+const controls = new OrbitControls(camera, renderer.domElement);
+controls.enableDamping = true;
 
 // World
-let gravity = { x: 0.0, y: 0, z: 0.0 };
+let accent = 0;
 
 const world = new World(gravity);
-const debugs = new LineSegments(
-  new BufferGeometry(),
-  new LineBasicMaterial({ color: 0xffffff, vertexColors: true }),
-);
-debugs.frustumCulled = false;
-debugs.visible = false;
-scene.add(debugs);
+const debug = new LineSegments(new BufferGeometry(), new LineBasicMaterial());
+debug.frustumCulled = false;
+scene.add(debug);
 
-const floorGeometry = new PlaneGeometry(10, 10, 16, 16);
-const floorMaterial = new ShadowMaterial({
-  transparent: true,
-});
-const floor = new Mesh(floorGeometry, floorMaterial);
-floor.rotation.x = -Math.PI / 2;
-floor.receiveShadow = true;
-scene.add(floor);
-
-const floorRigidBodyDesc = RigidBodyDesc.fixed();
-floorRigidBodyDesc.setTranslation(0, -0.1, 0);
-const floorRigidBody = world.createRigidBody(floorRigidBodyDesc);
-
-const floorColliderDesc = ColliderDesc.cuboid(5, 0.1, 5);
-world.createCollider(floorColliderDesc, floorRigidBody);
-
-const gridMaterial = new MeshBasicMaterial({
-  color: Colors.VIOLET3,
-  wireframe: true,
-});
-const grid = new Mesh(floorGeometry, gridMaterial);
-grid.rotation.x = -Math.PI / 2;
-scene.add(grid);
-
-const ballGeometry = new DodecahedronGeometry(0.5, 0);
+const sphereGeometry = new SphereGeometry(1, 64, 64);
+const sphereMaterial = new MeshBasicMaterial({ color: 'yellow' });
+const sphere = new Mesh(sphereGeometry, sphereMaterial);
+scene.add(sphere);
 
 const ballRigidBodyDesc = RigidBodyDesc.dynamic();
-ballRigidBodyDesc.setTranslation(0, 1, 0);
+ballRigidBodyDesc.setTranslation(0, 3, 0);
 const ballRigidBody = world.createRigidBody(ballRigidBodyDesc);
 
-const ballColliderDesc = ColliderDesc.convexHull(
-  ballGeometry.getAttribute('position').array as Float32Array,
-)!;
-ballColliderDesc.setMass(2);
-ballColliderDesc.setFriction(0.5);
-ballColliderDesc.setRestitution(0.875);
+const ballColliderDesc = ColliderDesc.ball(1.03);
+ballColliderDesc.setRestitution(0.856);
 world.createCollider(ballColliderDesc, ballRigidBody);
 
-const ballMaterial = new MeshPhysicalMaterial({});
-const ball = new Mesh(ballGeometry, ballMaterial);
-ball.castShadow = true;
-ball.position.y = 0.5;
-ball.updateMatrixWorld(true);
-scene.add(ball);
+const floorRigidBodyDesc = RigidBodyDesc.fixed();
+const floorRigidBody = world.createRigidBody(floorRigidBodyDesc);
 
-const meshs = {
-  [ball.uuid]: {
-    mesh: ball,
-    body: ballRigidBody,
-  },
-};
+const floorColliderDesc = ColliderDesc.cuboid(10, 0.1, 10);
+world.createCollider(floorColliderDesc, floorRigidBody);
 
-const decals = [
-  {
-    name: 'react',
-    position: new Vector3(-0.03, 0.87, 0.09),
-    orientation: new Euler(-1.0, 0, 0, 'XYZ'),
-    size: new Vector3(0.15, 0.15, 0.15),
-    texture: reactTexture,
-  },
-  {
-    name: 'react',
-    position: new Vector3(0.21, 0.83, 0.12),
-    orientation: new Euler(-1.03, 0.46, -0.26, 'XYZ'),
-    size: new Vector3(0.15, 0.15, 0.15),
-    texture: threeTexture,
-  },
-];
-
-function createDecals() {
-  for (const decal of decals) {
-    const geometry = new DecalGeometry(ball, decal.position, decal.orientation, decal.size);
-    const material = new MeshPhysicalMaterial({
-      map: decal.texture,
-      transparent: true,
-      polygonOffset: true,
-      polygonOffsetFactor: -4,
-      depthTest: true,
-      depthWrite: false,
-      metalness: 0.7,
-      thickness: 0,
-      ior: 1.5,
-      iridescence: 1,
-      iridescenceIOR: 1,
-      iridescenceThicknessRange: [300, 1500],
-    });
-    const mesh = new Mesh(geometry, material);
-    mesh.position.sub(ball.position);
-    ball.add(mesh);
-  }
-}
-
-createDecals();
-
-const stickerReactMaterial = new MeshPhysicalMaterial({
-  map: threeTexture,
-  transparent: true,
-  polygonOffset: true,
-  polygonOffsetFactor: -4,
-  depthTest: true,
-  depthWrite: false,
-  metalness: 0.7,
-  thickness: 0,
-  ior: 1.5,
-  iridescence: 1,
-  iridescenceIOR: 1,
-  iridescenceThicknessRange: [300, 1500],
-});
-let stickerReact: Mesh | undefined;
-
-const params = {
-  position: new Vector3(-0.03, 0.87, 0.09),
-  orientation: new Vector3(-1.0, 0, 0),
-  iridescenceThicknessRange: { min: 300, max: 1500 },
-};
-
-function createDecalReact(_params: typeof params) {
-  if (stickerReact) {
-    stickerReact.geometry.dispose();
-    ball.remove(stickerReact);
-    stickerReact = undefined;
-  }
-  const geometry = new DecalGeometry(
-    ball,
-    _params.position,
-    new Euler().setFromVector3(_params.orientation),
-    new Vector3(0.15, 0.15, 0.15),
-  );
-  stickerReact = new Mesh(geometry, stickerReactMaterial);
-  stickerReact.position.sub(ball.position);
-  ball.add(stickerReact);
-}
-
-createDecalReact(params);
-
-const axesHelper = new AxesHelper();
+const axesHelper = new AxesHelper(5);
 scene.add(axesHelper);
 
-// Light
-const ambientLight = new AmbientLight(0xf3f3f3, 0.05);
-scene.add(ambientLight);
+// Events
 
-const directionLight = new DirectionalLight(0xf3f3f3, 5.35);
-directionLight.position.y = 30;
-directionLight.position.z = 20;
-directionLight.castShadow = true;
-directionLight.shadow.mapSize.set(4069, 4069);
-directionLight.shadow.needsUpdate = true;
-directionLight.shadow.blurSamples = 18;
-directionLight.shadow.radius = 30;
-scene.add(directionLight);
+function updateDebug() {
+  const { vertices, colors } = world.debugRender();
 
-const perf = new ThreePerf({
-  anchorX: 'left',
-  anchorY: 'top',
-  domElement: document.body,
-  renderer: renderer,
-  backgroundOpacity: 1,
-});
+  debug.geometry.setAttribute('position', new BufferAttribute(vertices, 3));
+  debug.geometry.setAttribute('color', new BufferAttribute(colors, 4));
+}
 
-const pane = new Pane({
-  title: 'Debug',
-});
-pane.registerPlugin(EssentialsPlugin);
+function updateTarget() {
+  sphere.position.copy(ballRigidBody.translation());
+  sphere.quaternion.copy(ballRigidBody.rotation());
+}
 
-pane.addBinding(params, 'position', { step: 0.01 }).on('change', () => {
-  createDecalReact(params);
-});
-pane.addBinding(params, 'orientation', { step: 0.01 }).on('change', () => {
-  createDecalReact(params);
-});
-
-pane.addBinding(stickerReactMaterial, 'ior', { min: 1, max: 2.333, step: 0.001 });
-pane.addBinding(stickerReactMaterial, 'iridescence', { min: 0, max: 1, step: 0.001 });
-pane.addBinding(stickerReactMaterial, 'iridescenceIOR', { min: 1, max: 2.333, step: 0.001 });
-pane.addBinding(params, 'iridescenceThicknessRange', { min: 100, max: 2000 });
-
-const speed = 5;
+function click() {
+  accent = ++accent % accents.length;
+}
 
 function render() {
-  perf.begin();
-
   // Update
-  timer.update();
-
-  controls1.update();
-  controls2.update();
-
-  const delta = timer.getDelta();
-  const elapsed = timer.getElapsed();
   world.step();
-
-  ball.position.copy(ballRigidBody.translation());
-  ball.quaternion.copy(ballRigidBody.rotation());
-
+  updateTarget();
+  controls.update();
   // Render
-  renderDebug();
+  updateDebug();
   renderer.render(scene, camera);
-  perf.end();
-  // Animation
+  //Animation
   requestAnimationFrame(render);
 }
+
 render();
 
 window.addEventListener('resize', () => {
@@ -315,29 +139,4 @@ window.addEventListener('resize', () => {
   camera.updateProjectionMatrix();
 });
 
-window.addEventListener('pointerdown', (e) => {
-  mouse.x = (e.clientX / sizes.width) * 2 - 1;
-  mouse.y = -(e.clientY / sizes.height) * 2 + 1;
-
-  raycaster.setFromCamera(mouse, camera);
-
-  const intersect = raycaster.intersectObject(ball);
-
-  if (intersect.length) {
-    point.copy(intersect[0].point);
-
-    const object = meshs[intersect[0].object.uuid];
-
-    if (object) {
-      object.body.setLinvel({ x: Math.random() > 0.5 ? 1 : -1, y: 3, z: 0 }, true);
-      object.body.setAngvel({ x: Math.random() > 0.5 ? 3 : -3, y: 0, z: 0 }, true);
-    }
-  }
-});
-
-function renderDebug() {
-  const { vertices, colors } = world.debugRender();
-
-  debugs.geometry.setAttribute('position', new BufferAttribute(vertices, 3));
-  debugs.geometry.setAttribute('color', new BufferAttribute(colors, 4));
-}
+window.addEventListener('pointerdown', click);
